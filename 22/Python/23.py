140bc8db8491ab49bc0e62df563e8cb41688f09a
GoatedChopin <colbysmeline@gmail.com>
2022-12-24 20:33:14 -0600

    Day 23 fully functional!

--- 22/Python/23.py
+++ 22/Python/23.py
@@ -1,20 +1,31 @@
 import os
+from collections import deque
 
 
-with open("inputs" + os.sep + "23.txt") as file:
-    inputs = file.readlines()
-    inputs = [[c for c in line if c != "\n"] for line in inputs]
+def load_elves():
+    with open("inputs" + os.sep + "23.txt") as file:
+        inputs = file.readlines()
+        inputs = [[c for c in line if c != "\n"] for line in inputs]
+    elves = set()
+    for row in range(len(inputs)):
+        for col in range(len(inputs[0])):
+            if inputs[row][col] == "#":
+                elves.add((row, col))
+    return elves
 
 
-elves = set()
-for row in range(len(inputs)):
-    for col in range(len(inputs[0])):
-        if inputs[row][col] == "#":
-            elves.add((row, col))
+def shift(coord, delta):
+    row, col = coord
+    rd, cd = delta
+    return row + rd, col + cd
 
 
-def desired_destination(elf_coord):
+def desired_destination(elf_coord, round=0):
     global elves
+
+    movements = deque([(-1, 0), (1, 0), (0, -1), (0, 1)])
+    movements.rotate(-round)
+    
     row, col = elf_coord
     north_neighbors = 0
     south_neighbors = 0
@@ -33,19 +44,19 @@ def desired_destination(elf_coord):
                     east_neighbors += 1
     if sum([north_neighbors, south_neighbors, west_neighbors, east_neighbors]) == 0:
         return elf_coord
-    elif north_neighbors == 0:
-        return (row - 1, col)
-    elif south_neighbors == 0:
-        return (row + 1, col)
-    elif west_neighbors == 0:
-        return (row, col - 1)
-    elif east_neighbors == 0:
-        return (row, col + 1)
-    else:
-        return elf_coord
 
 
-def round(elves):
+    neighbor_deque = deque([north_neighbors, south_neighbors, west_neighbors, east_neighbors])
+    neighbor_deque.rotate(-round)
+
+
+    for i in range(4):
+        if neighbor_deque[i] == 0:
+            return shift(elf_coord, movements[i])
+    return elf_coord
+
+
+def round(elves, id=0):
 
     blocked_tiles, open_tiles = 0, 0
 
@@ -53,7 +64,7 @@ def round(elves):
     proposed_positions = {}  # elf -> proposed_move
     proposal_counts = {}  # proposed_move -> int
     for elf in elves:
-        proposed_move = desired_destination(elf)
+        proposed_move = desired_destination(elf, id)
         proposed_positions[elf] = proposed_move
         if proposed_move not in proposal_counts:
             proposal_counts[proposed_move] = 0
@@ -66,7 +77,7 @@ def round(elves):
         else:
             blocked_tiles += 1
             new_elves.add(elf)
-    print("{}% of positions were open".format(100*open_tiles/sum([open_tiles, blocked_tiles])))
+    # print("{}% of positions were open".format(100*open_tiles/sum([open_tiles, blocked_tiles])))
     return new_elves
 
 
@@ -96,11 +107,27 @@ def smallest_rectangle_empty_tiles(elves):
 
 def part_one():
     global elves
-    for _ in range(10):
-        elves = round(elves)
+    for id in range(10):
+        elves = round(elves, id)
     empty_tiles = smallest_rectangle_empty_tiles(elves)
     return empty_tiles
 
 
+def part_two():
+    global elves
+    new_elves = elves
+    round_id = 0
+    while True:
+        new_elves = round(elves, round_id)
+        round_id += 1
+        if new_elves == elves:
+            break
+        elves = new_elves
+    return round_id
+
+
 if __name__ == "__main__":
+    elves = load_elves()
     print(part_one())
+    elves = load_elves()
+    print(part_two())
