4d3aaddaf05e4dccccf9e0b5e1d3efaaef3bd5a4
Colby Meline <colbymeline@mac.lan>
2025-04-24 20:43:36 -0600

    Done with 17

--- 16/Go/day17/main.go
+++ 16/Go/day17/main.go
@@ -10,35 +10,36 @@ import (
 type Vec struct {
 	x     int
 	y     int
-	label rune
-}
-
-func move(v Vec, d Vec) Vec {
-	return Vec{v.x + d.x, v.y + d.y, d.label}
-}
-
-func inBounds(v Vec) bool {
-	if 0 <= v.x && v.x < 4 && 0 <= v.y && v.y < 4 {
-		return true
-	}
-	return false
+	label byte
 }
 
 type Path struct {
-	fullPath string // passcode + directions
+	path     []byte // just the directions, not the full passcode
 	position Vec
 }
 
-func Unlocked(fullPath string, position Vec) []Vec {
-	sum := md5.Sum([]byte(fullPath))
+func inBounds(v Vec) bool {
+	return 0 <= v.x && v.x <= 3 && 0 <= v.y && v.y <= 3
+}
+
+var unlockChars = [256]bool{
+	'b': true, 'c': true, 'd': true, 'e': true, 'f': true,
+}
+
+var dirs = [4]Vec{{0, -1, 'U'}, {0, 1, 'D'}, {-1, 0, 'L'}, {1, 0, 'R'}}
+
+func Unlocked(passcode string, path []byte, position Vec) []Vec {
+	buf := make([]byte, len(passcode)+len(path))
+	copy(buf, passcode)
+	copy(buf[len(passcode):], path)
+	sum := md5.Sum(buf)
 	hash := hex.EncodeToString(sum[:])
-	unlockChars := map[byte]bool{'b': true, 'c': true, 'd': true, 'e': true, 'f': true}
-	dirs := []Vec{{0, -1, 'U'}, {0, 1, 'D'}, {-1, 0, 'L'}, {1, 0, 'R'}}
+
 	var out []Vec
 	for i := 0; i < 4; i++ {
 		if unlockChars[hash[i]] {
 			newPos := Vec{position.x + dirs[i].x, position.y + dirs[i].y, dirs[i].label}
-			if 0 <= newPos.x && newPos.x < 4 && 0 <= newPos.y && newPos.y < 4 {
+			if inBounds(newPos) {
 				out = append(out, dirs[i])
 			}
 		}
@@ -46,46 +47,50 @@ func Unlocked(fullPath string, position Vec) []Vec {
 	return out
 }
 
-func Pathfind(s string) string {
-	queue := list.New()
-	queue.PushBack(Path{s, Vec{0, 0, ' '}})
-	for queue.Len() > 0 {
-		front := queue.Front()
-		path := queue.Remove(front).(Path)
-		if path.position.x == 3 && path.position.y == 3 {
-			return path.fullPath[len(s):]
-		}
-		for _, dir := range Unlocked(path.fullPath, path.position) {
-			queue.PushBack(Path{path.fullPath + string(dir.label), Vec{path.position.x + dir.x, path.position.y + dir.y, dir.label}})
-		}
-	}
-	return ""
-}
-
-func LongestPathfind(s string) int {
+func LongestPathfind(passcode string) int {
 	max := 0
-	queue := list.New()
-	queue.PushBack(Path{s, Vec{0, 0, ' '}})
-	for queue.Len() > 0 {
-		front := queue.Front()
-		path := queue.Remove(front).(Path)
+	queue := []Path{{[]byte{}, Vec{0, 0, 0}}}
+
+	for len(queue) > 0 {
+		path := queue[0]
+		queue = queue[1:]
+
 		if path.position.x == 3 && path.position.y == 3 {
-			if newMax := len(path.fullPath[len(s):]); newMax > max {
-				fmt.Printf("New longest: %v\n", newMax)
+			if newMax := len(path.path); newMax > max {
 				max = newMax
-				continue
 			}
+			continue
 		}
-		for _, dir := range Unlocked(path.fullPath, path.position) {
-			queue.PushBack(Path{path.fullPath + string(dir.label), Vec{path.position.x + dir.x, path.position.y + dir.y, dir.label}})
+
+		for _, dir := range Unlocked(passcode, path.path, path.position) {
+			newPath := make([]byte, len(path.path)+1)
+			copy(newPath, path.path)
+			newPath[len(path.path)] = dir.label
+			queue = append(queue, Path{newPath, Vec{path.position.x + dir.x, path.position.y + dir.y, dir.label}})
 		}
 	}
 	return max
+}
+
+func Pathfind(s string) string {
+	queue := list.New()
+	queue.PushBack(Path{[]byte(s), Vec{0, 0, 0}})
+	for queue.Len() > 0 {
+		front := queue.Front()
+		path := queue.Remove(front).(Path)
+		if path.position.x == 3 && path.position.y == 3 {
+			return string(path.path[len(s):])
+		}
+		for _, dir := range Unlocked(s, path.path, path.position) {
+			queue.PushBack(Path{path.path, dir})
+		}
+	}
+	return ""
 }
 
 func main() {
 	fmt.Print("Starting day 15\n")
 	input := "udskfozm"
 	part1 := LongestPathfind(input)
-	fmt.Printf("Part 1: %v", part1)
+	fmt.Printf("Part 1: %v\n", part1)
 }
