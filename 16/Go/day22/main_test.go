684d0c6f751109a03f7b27479e0d94f419f38e30
Colby Meline <colbymeline@mac.lan>
2025-04-26 22:08:56 -0600

    Removing unused code

--- 16/Go/day22/main_test.go
+++ 16/Go/day22/main_test.go
@@ -4,20 +4,4 @@ import (
 	"testing"
 )
 
-func Test_dayFourteen00(t *testing.T) {
-	// t.Run("LowestIp", func(t *testing.T) {
-	// 	if got := LowestAllowedIp([]string{"5-8", "0-2", "4-7"}); got != 3 {
-	// 		t.Errorf("%v = %v, want %v", "5", got, 3)
-	// 	}
-	// })
-	// t.Run("LowestIp", func(t *testing.T) {
-	// 	if got := LowestAllowedIp(adv.GetInput("20", true, "\n", true)); got != 3 {
-	// 		t.Errorf("%v = %v, want %v", "5", got, 3)
-	// 	}
-	// })
-	t.Run("All IPs", func(t *testing.T) {
-		if got := MinMoves([]Node{}); got != 3 {
-			t.Errorf("%v = %v, want %v", "5", got, 3)
-		}
-	})
-}
+func Test(t *testing.T) {}
