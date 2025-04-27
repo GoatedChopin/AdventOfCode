4a326dd267a09011bb8beec5b57b546ed56ca4a7
Colby Meline <colbymeline@mac.lan>
2025-04-27 16:56:12 -0600

    Day 25

--- 16/Go/day25/main.go
+++ 16/Go/day25/main.go
@@ -8,17 +8,15 @@ import (
 	adv "github.com/GoatedChopin/AdventOfCode/16/Go/util"
 )
 
-var Registers map[string]int = make(map[string]int)
-
-func Init() {
-	Registers["a"] = 0
-	Registers["b"] = 0
-	Registers["c"] = 0
-	Registers["d"] = 0
+func Init(r *map[string]int) {
+	(*r)["a"] = 0
+	(*r)["b"] = 0
+	(*r)["c"] = 0
+	(*r)["d"] = 0
 }
 
-func getOrLookup(s string) int {
-	v, ok := Registers[s]
+func getOrLookup(s string, r *map[string]int) int {
+	v, ok := (*r)[s]
 	if ok {
 		return v
 	}
@@ -29,15 +27,15 @@ func getOrLookup(s string) int {
 	return c
 }
 
-func Execute(instruction []string) (int, int) {
+func Execute(instruction []string, r *map[string]int) (int, int) {
 	switch instruction[0] {
 	case "inc":
-		Registers[instruction[1]]++
+		(*r)[instruction[1]]++
 	case "dec":
-		Registers[instruction[1]]--
+		(*r)[instruction[1]]--
 	case "jnz":
-		// Check that registers has instruction[1] and that it's not 0 OR that it's a nonzero number
-		value, ok := Registers[instruction[1]]
+		// Check that (*r) has instruction[1] and that it's not 0 OR that it's a nonzero number
+		value, ok := (*r)[instruction[1]]
 		if (ok && value != 0) || (!ok && instruction[1] != "0") {
 			jump, err := strconv.Atoi(instruction[2])
 			if err != nil {
@@ -47,19 +45,19 @@ func Execute(instruction []string) (int, int) {
 		}
 	case "cpy":
 		a, b := instruction[1], instruction[2]
-		v1, ok1 := Registers[a]
-		_, ok2 := Registers[b]
+		v1, ok1 := (*r)[a]
+		_, ok2 := (*r)[b]
 		if ok1 && ok2 {
-			Registers[b] = v1
+			(*r)[b] = v1
 		} else if ok2 {
 			value, err := strconv.Atoi(a)
 			if err != nil {
 				panic("BAD INT VALUE AT cpy")
 			}
-			Registers[b] = value
+			(*r)[b] = value
 		}
 	case "out":
-		v := getOrLookup(instruction[1])
+		v := getOrLookup(instruction[1], r)
 		return 0, v
 	default:
 		return 0, -1
@@ -67,22 +65,18 @@ func Execute(instruction []string) (int, int) {
 	return 0, -1
 }
 
-func Follow(ch chan int, aVal int, instructions []string, part int) <-chan int {
-	ch = make(chan int)
-	func(ch chan int) {
-		Init()
-		Registers["a"] = aVal
-		splitInstructions := make([][]string, len(instructions))
-		for i, s := range instructions {
-			args := strings.Split(s, " ")
-			splitInstructions[i] = args
-		}
+func Follow(aVal int, splitInstructions [][]string) <-chan int {
+	var registers map[string]int = make(map[string]int)
+	ch := make(chan int)
+	go func(ch chan int) {
+		defer close(ch)
+		Init(&registers)
+		registers["a"] = aVal
 		i := 0
-		fmt.Printf("Running instructions: %v\n", instructions)
-		for i < len(instructions) {
+		for i < len(splitInstructions) {
 			// fmt.Printf("%v -> %v\n", instructions[i], Registers)
 			args := splitInstructions[i]
-			jump, out := Execute(args)
+			jump, out := Execute(args, &registers)
 			if args[0] == "out" {
 				ch <- out
 			}
@@ -96,11 +90,50 @@ func Follow(ch chan int, aVal int, instructions []string, part int) <-chan int {
 	return ch
 }
 
+func AlternatingSignal(s []int) bool {
+	for i := range len(s) - 1 {
+		b := s[i]
+		opp := 0
+		if b == 0 {
+			opp = 1
+		} else if b == 1 {
+			opp = 0
+		} else {
+			return false
+		}
+		if s[i+1] != opp {
+			return false
+		}
+	}
+	return true
+}
+
+func FindLowestSignal(instructions []string) int {
+	l := 0
+	splitInstructions := make([][]string, len(instructions))
+	for i, s := range instructions {
+		args := strings.Split(s, " ")
+		splitInstructions[i] = args
+	}
+	for {
+		buff := make([]int, 0)
+		for ni := range Follow(l, splitInstructions) {
+			buff = append(buff, ni)
+			if len(buff) >= 20 {
+				break
+			}
+		}
+		if AlternatingSignal(buff) {
+			break
+		}
+		l++
+	}
+	return l
+}
+
 func main() {
-	fmt.Print("Starting day 12\n")
-	inputs := adv.GetInput("12", true, "\n", true)
-	part1 := Follow(inputs, 0)
+	fmt.Print("Starting day 25\n")
+	inputs := adv.GetInput("25", true, "\n", true)
+	part1 := FindLowestSignal(inputs)
 	fmt.Printf("Part 1: %v\n", part1)
-	part2 := Follow(inputs, 1)
-	fmt.Printf("Part 2: %v\n", part2)
 }
