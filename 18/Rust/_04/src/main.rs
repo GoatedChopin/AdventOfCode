d331dd6f52a21ae459e6aa7e1b1c371f7653230d
Colby Meline <colby.meline@webbank.com>
2026-04-22 10:01:15 -0600

    Nearly there

--- 18/Rust/_04/src/main.rs
+++ 18/Rust/_04/src/main.rs
@@ -1,146 +1,65 @@
-use chrono::DateTime;
-use std::{collections::HashMap, fs};
+use chrono::{NaiveDateTime, TimeDelta, Timelike};
+use std::{collections::{HashMap, HashSet}, fs};
 
-const MINUTES_TO_SECONDS: isize = 60;
-const HOURS_TO_SECONDS: isize = MINUTES_TO_SECONDS * 60;
-const DAYS_TO_SECONDS: isize = HOURS_TO_SECONDS * 24;
-const MONTHS_TO_SECONDS: isize = DAYS_TO_SECONDS * 30;
-const YEARS_TO_SECONDS: isize = MONTHS_TO_SECONDS * 12;
-
-#[derive(Clone, Copy)]
-struct TimeStamp {
-    year: isize,
-    month: isize,
-    day: isize,
-    hour: isize,
-    minute: isize,
-}
-
-impl TimeStamp {
-    fn from_str(s: &str) -> Self {
-        let binding = s
-            .replace("[", "")
-            .replace("]", "")
-            .replace("-", " ")
-            .replace(":", " ");
-        let parts: Vec<&str> = binding.split_whitespace().collect();
-
-        let year = isize_from_leading_zero(parts[0]);
-        let month = isize_from_leading_zero(parts[1]);
-        let day = isize_from_leading_zero(parts[2]);
-        let hour = isize_from_leading_zero(parts[3]);
-        let minute = isize_from_leading_zero(parts[4]);
-
-        Self {
-            year,
-            month,
-            day,
-            hour,
-            minute,
-        }
+fn minute_counts_between(start: &NaiveDateTime, end: &NaiveDateTime) -> HashMap<u32, usize> {
+    let mut counts = HashMap::new();
+    if start >= end {
+        return counts;
     }
 
-    fn diff(self, other: &TimeStamp) -> isize {
-        let year_diff = (self.year - other.year) * YEARS_TO_SECONDS;
-        let month_diff = (self.month - other.month) * MONTHS_TO_SECONDS;
-        let day_diff = (self.day - other.day) * DAYS_TO_SECONDS;
-        let hour_diff = (self.hour - other.hour) * HOURS_TO_SECONDS;
-        let minute_diff = (self.minute - other.minute) * MINUTES_TO_SECONDS;
-
-        year_diff + month_diff + day_diff + hour_diff + minute_diff
-    }
-
-    fn minute_counts_between(self, other: &TimeStamp) -> HashMap<isize, isize> {
-        let mut counts = HashMap::new();
-        if self >= *other {
-            return counts;
-        }
-
-        let mut 
-        while 
-
-        counts
-    }
-}
-
-fn isize_from_leading_zero(s: &str) -> isize {
-    let chars: Vec<char> = s.chars().collect();
-    let mut i = 0;
-    while i < chars.len() && chars[i] == '0' {
-        i += 1;
-    }
-    let slice = s.split_at_checked(i);
-    match slice {
-        Some(parts) => {
-            if parts.1.len() == 0 {
-                return 0;
+    let mut current = start.clone();
+    let mut current_minute = current.minute();
+    while current_minute != 0 && current < *end {
+        let min = counts.get_mut(&current_minute);
+        match min {
+            Some(min) => *min += 1,
+            None => {
+                counts.insert(current_minute, 1);
             }
-            return parts.1.parse().unwrap();
         }
-        None => {
-            return 0;
+        current += TimeDelta::new(60, 0).expect("Bad TimeDelta");
+        current_minute = current.minute();
+    }
+
+    let mut last = start.clone();
+    let mut last_minute = last.minute();
+    while last_minute != 0 && last > current {
+        let min = counts.get_mut(&last_minute);
+        match min {
+            Some(min) => *min += 1,
+            None => {
+                counts.insert(last_minute, 1);
+            }
+        }
+        last -= TimeDelta::new(60, 0).expect("Bad TimeDelta");
+        last_minute = last.minute();
+    }
+
+    let gap = last - current;
+
+    if gap.num_hours() == 0 {
+        return counts;
+    }
+
+    let minutes = gap.num_minutes();
+    for i in 0..60 {
+        let minute_count = counts.get_mut(&i);
+        match minute_count {
+            Some(c) => {
+                *c += minutes as usize;
+            }
+            None => {
+                counts.insert(i, minutes as usize);
+            }
         }
     }
-}
 
-impl Ord for TimeStamp {
-    fn cmp(&self, other: &Self) -> std::cmp::Ordering {
-        if self.year != other.year {
-            return self.year.cmp(&other.year);
-        }
-        if self.month != other.month {
-            return self.month.cmp(&other.month);
-        }
-        if self.day != other.day {
-            return self.day.cmp(&other.day);
-        }
-        if self.hour != other.hour {
-            return self.hour.cmp(&other.hour);
-        }
-        if self.minute != other.minute {
-            return self.minute.cmp(&other.minute);
-        }
-        return std::cmp::Ordering::Equal;
-    }
+    counts
 }
-
-impl PartialOrd for TimeStamp {
-    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
-        match self.year.partial_cmp(&other.year) {
-            Some(core::cmp::Ordering::Equal) => {}
-            ord => return ord,
-        }
-        match self.month.partial_cmp(&other.month) {
-            Some(core::cmp::Ordering::Equal) => {}
-            ord => return ord,
-        }
-        match self.day.partial_cmp(&other.day) {
-            Some(core::cmp::Ordering::Equal) => {}
-            ord => return ord,
-        }
-        match self.hour.partial_cmp(&other.hour) {
-            Some(core::cmp::Ordering::Equal) => {}
-            ord => return ord,
-        }
-        self.minute.partial_cmp(&other.minute)
-    }
-}
-
-impl PartialEq for TimeStamp {
-    fn eq(&self, other: &Self) -> bool {
-        return self.year == other.year
-            && self.month == other.month
-            && self.day == other.day
-            && self.hour == other.hour
-            && self.minute == other.minute;
-    }
-}
-
-impl Eq for TimeStamp {}
 
 #[derive(Clone, Copy)]
 struct EventMember {
-    timestamp: TimeStamp,
+    datetime: NaiveDateTime,
     guard_id: isize,
 }
 
@@ -153,21 +72,20 @@ enum Event {
 
 impl Event {
     fn from_str(s: &str, guard_id: Option<isize>) -> Self {
-        let (timestamp_str, rest) = s.split_once("]").expect("No timestamp in string");
-        let timestamp = TimeStamp::from_str(timestamp_str);
+        let (datetime_str, rest) = s.split_once("]").expect("No NaiveDateTime in string");
+        // println!("datetime_str: {}", datetime_str);
+        let datetime = NaiveDateTime::parse_from_str(datetime_str, "[%Y-%m-%d %H:%M")
+            .expect("Bad datetime string");
 
         let rest_parts: Vec<&str> = rest.trim().split_whitespace().collect();
         match rest_parts[0] {
             "Guard" => {
                 let guard_id = rest_parts[1].replace("#", "").parse().unwrap();
-                return Self::BeginsShift(EventMember {
-                    timestamp,
-                    guard_id,
-                });
+                return Self::BeginsShift(EventMember { datetime, guard_id });
             }
             "falls" => {
                 return Self::FallsAsleep(EventMember {
-                    timestamp,
+                    datetime,
                     guard_id: guard_id.expect(
                         "guard_id must be provided when event type is WakesUp or FallsAsleep",
                     ),
@@ -175,7 +93,7 @@ impl Event {
             }
             "wakes" => {
                 return Self::WakesUp(EventMember {
-                    timestamp,
+                    datetime,
                     guard_id: guard_id.expect(
                         "guard_id must be provided when event type is WakesUp or FallsAsleep",
                     ),
@@ -194,11 +112,11 @@ impl Event {
         }
     }
 
-    fn timestamp(self) -> TimeStamp {
+    fn datetime(self) -> NaiveDateTime {
         match self {
-            Event::BeginsShift(event_member) => event_member.timestamp,
-            Event::FallsAsleep(event_member) => event_member.timestamp,
-            Event::WakesUp(event_member) => event_member.timestamp,
+            Event::BeginsShift(event_member) => event_member.datetime,
+            Event::FallsAsleep(event_member) => event_member.datetime,
+            Event::WakesUp(event_member) => event_member.datetime,
         }
     }
 }
@@ -221,19 +139,65 @@ fn read_input(input_file: &str) -> Vec<Event> {
     events
 }
 
-fn part_one(events: Vec<Event>) -> isize {
-    let mut guards = HashMap::new();
-    let mut previous_timestamp = events[0].timestamp();
+fn get_best_minute(events: &Vec<Event>) -> (i32, usize) {
+    let mut minute_counts: HashMap<u32, usize> = HashMap::new();
+    let mut current_datetime = events[0].datetime();
     for event in events.iter() {
-        let time_diff = event.timestamp().diff(&previous_timestamp);
-        previous_timestamp = event.timestamp();
+        match event {
+            Event::BeginsShift(_) => {}
+            Event::FallsAsleep(event_member) => {
+                current_datetime = event_member.datetime;
+            }
+            Event::WakesUp(event_member) => {
+                // println!(
+                //     "Guard slept from {} to {}",
+                //     current_datetime, event_member.datetime
+                // );
+                let sub_minute_counts =
+                    minute_counts_between(&current_datetime, &event_member.datetime);
+                sub_minute_counts.iter().for_each(|(minute, count)| {
+                    let mcount = minute_counts.get_mut(minute);
+                    match mcount {
+                        Some(c) => {
+                            // println!("Adding {} to {}", count, c);
+                            *c += count;
+                        }
+                        None => {
+                            minute_counts.insert(*minute, *count);
+                        }
+                    }
+                });
+            }
+        }
+    }
+    let (best_minute, minute_count) =
+        minute_counts
+            .iter()
+            .fold((0, 0), |acc, (minute, minute_count)| {
+                if *minute_count > acc.1 {
+                    return (*minute as i32, *minute_count);
+                }
+                return acc;
+            });
+    (best_minute, minute_count)
+}
+
+fn part_one(events: Vec<Event>) -> i32 {
+    let mut guards: HashMap<isize, TimeDelta> = HashMap::new();
+    let mut previous_timestamp = events[0].datetime();
+    for event in events.iter() {
+        let time_diff = event.datetime() - previous_timestamp;
+        previous_timestamp = event.datetime();
         match event {
             Event::BeginsShift(_) => {
                 let guard_sleep = guards.get_mut(&event.guard_id());
                 match guard_sleep {
-                    Some(_) => {},
+                    Some(_) => {}
                     None => {
-                        guards.insert(event.guard_id(), 0);
+                        guards.insert(
+                            event.guard_id(),
+                            TimeDelta::new(0, 0).expect("Bad TimeDelta"),
+                        );
                     }
                 }
             }
@@ -248,42 +212,89 @@ fn part_one(events: Vec<Event>) -> isize {
         }
     }
     let mut max_sleeper_guard_id = 0;
-    guards.into_iter().fold(0, |acc, (guard_id, time_slept)| {
-        if time_slept > acc {
-            max_sleeper_guard_id = guard_id;
-            return time_slept;
-        }
-        return acc;
-    });
-    
+    guards.into_iter().fold(
+        TimeDelta::new(0, 0).expect("Bad TimeDelta"),
+        |acc, (guard_id, time_slept)| {
+            if time_slept > acc {
+                max_sleeper_guard_id = guard_id;
+                return time_slept;
+            }
+            return acc;
+        },
+    );
+
+    println!("Max guard sleeper is {}", max_sleeper_guard_id);
+
     // Compute a per-minute breakdown for this particular guard.
-    let mut minute_counts = HashMap::new();
-    let filtered_events: Vec<Event> = events.iter().filter(|event| event.guard_id() == max_sleeper_guard_id).collect();
-    let mut current_timestamp = filtered_events[0].timestamp();
+    let mut minute_counts: HashMap<u32, usize> = HashMap::new();
+    let filtered_events: Vec<Event> = events
+        .into_iter()
+        .filter(|event| event.guard_id() == max_sleeper_guard_id)
+        .collect();
+    let mut current_datetime = filtered_events[0].datetime();
     for event in filtered_events.iter() {
         match event {
-            Event::BeginsShift(event_member) => {},
+            Event::BeginsShift(_) => {}
             Event::FallsAsleep(event_member) => {
-                current_timestamp = event_member.timestamp;
-            },
+                current_datetime = event_member.datetime;
+            }
             Event::WakesUp(event_member) => {
-                event_member.timestamp.for_each_minute_between(current_timestamp, |minute| {
+                // println!(
+                //     "Guard slept from {} to {}",
+                //     current_datetime, event_member.datetime
+                // );
+                let sub_minute_counts =
+                    minute_counts_between(&current_datetime, &event_member.datetime);
+                sub_minute_counts.iter().for_each(|(minute, count)| {
                     let mcount = minute_counts.get_mut(minute);
                     match mcount {
-                        Some(c) => { *c += 1 }
-                        None => { minute_counts.insert(minute, 1); }
+                        Some(c) => {
+                            // println!("Adding {} to {}", count, c);
+                            *c += count;
+                        }
+                        None => {
+                            minute_counts.insert(*minute, *count);
+                        }
                     }
                 });
-            },
+            }
         }
     }
-    let best_minute = 0;
-    best_minute;
+    let (best_minute, _minute_count) =
+        minute_counts
+            .iter()
+            .fold((0, 0), |acc, (minute, minute_count)| {
+                if *minute_count > acc.1 {
+                    return (*minute as i32, *minute_count);
+                }
+                return acc;
+            });
+
+    (max_sleeper_guard_id as i32) * best_minute
+}
+
+fn part_two(events: Vec<Event>) -> isize {
+    let guard_ids: HashSet<isize> = events.iter().map(|event| event.guard_id()).collect();
+    let (mut best_guard_id, mut best_minute, mut best_minute_count) = (0, 0, 0);
+    for guard_id in guard_ids {
+        println!("Checking guard {}", guard_id);
+        let filtered_events = events.iter().filter(|event| event.guard_id() == guard_id).map(|event| event.clone()).collect();
+        let (minute, count) = get_best_minute(&filtered_events);
+        if count > best_minute_count {
+            println!("\tNew best: {} slept {} times on minute {}", guard_id, count, minute);
+            best_guard_id = guard_id;
+            best_minute = minute;
+            best_minute_count = count;
+        }
+    }
+
+    best_guard_id * best_minute as isize
 }
 
 fn main() {
     let input = read_input("input.txt");
-    println!("Part one: {}", part_one(input));
+    println!("Part one: {}", part_one(input.clone()));
+    println!("Part two: {}", part_two(input));
 }
 
 #[cfg(test)]
@@ -291,18 +302,14 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_parse_leading_zeros() {
-        let ex1 = "00";
-        assert_eq!(isize_from_leading_zero(ex1), 0);
-        let ex2 = "01";
-        assert_eq!(isize_from_leading_zero(ex2), 1);
-        let ex3 = "011";
-        assert_eq!(isize_from_leading_zero(ex3), 11);
-    }
-
-    #[test]
     fn test_part_one() {
         let input = read_input("test.txt");
         assert_eq!(part_one(input), 240);
+    }
+
+    #[test]
+    fn test_part_two() {
+        let input = read_input("test.txt");
+        assert_eq!(part_two(input), 4455);
     }
 }
